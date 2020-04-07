68aa97223af1c754dd6565411d9b58921ad56c7b
Markus Helbig <mhelbig81@googlemail.com>
2020-04-07 11:20:05 +0000

    version 1.8-7

--- R/JGR.R
+++ R/JGR.R
@@ -1,6 +1,6 @@
 #==========================================================================
 # JGR - Java Gui for R
-# Package version: 1.8-6
+# Package version: 1.8-7
 #
 # (C)Copyright 2004-2017 Markus Helbig
 # (C)Copyright 2009-2013 Ian Fellows
